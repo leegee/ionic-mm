c6b3efcab1926766a06adb382f8067f11e17e478
Lee Goddard <leegee@gmail.com>
2017-12-05 10:06:36 +0100

    Late!

---
+++ src/components/text-block-interface.ts
@@ -0,0 +1,7 @@
+export declare abstract class TextBlockInterface {
+    abstract getText(): string;
+    abstract getX(): string;
+    abstract getY(): string;
+    abstract getClr(): string;
+    abstract getElement(): HTMLElement;
+}
